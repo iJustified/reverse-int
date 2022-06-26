0171fd829d71f4635a06cb985ce868e020707404
Ilia Tsygankov <ijustified@yandex.ru>
2022-06-26 21:26:45 +0300

    init: start reverse-int
    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let absNumber = Math.abs(n);
+  console.log(Math.abs(n));
+  console.log(Number(absNumber.toString().split('').reverse().join('')));
+  return Number(absNumber.toString().split('').reverse().join(''));
 }
